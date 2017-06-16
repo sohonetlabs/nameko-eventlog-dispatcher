cace35cf446152dc386527289fde18c63915fdf2
Julio Trigo <julio.trigo@gmail.com>
2017-06-16 16:59:00 +0100

    Use service name as source service when dispatching events

--- nameko_eventlog_dispatcher/eventlog_dispatcher.py
+++ nameko_eventlog_dispatcher/eventlog_dispatcher.py
@@ -2,11 +2,9 @@ import logging
 from copy import deepcopy
 from datetime import datetime, timezone
 
-from kombu.common import maybe_declare
-from nameko.amqp import get_connection, verify_amqp_uri
 from nameko.events import EventDispatcher
 from nameko.rpc import Rpc
-from nameko.standalone.events import event_dispatcher, get_event_exchange
+from nameko.standalone.events import event_dispatcher
 from nameko.web.handlers import HttpRequestHandler
 
 
@@ -33,9 +31,6 @@ class EventLogDispatcher(EventDispatcher):
     EVENT_TYPE = 'log_event'
     """Event type used by log events dispatched from the entrypoints."""
 
-    SOURCE_SERVICE = 'all'
-    """Source service name used when auto capturing events."""
-
     ENTRYPOINT_TYPES_TO_LOG = (Rpc, HttpRequestHandler)
     """Sequence of entrypoint types to auto capture event logs from."""
 
@@ -48,9 +43,6 @@ class EventLogDispatcher(EventDispatcher):
             'entrypoints_to_exclude'
         ) or []
 
-        if self.auto_capture and self.SOURCE_SERVICE is not None:
-            self._declare_additional_exchange()
-
     def worker_setup(self, worker_ctx):
         super().worker_setup(worker_ctx)
 
@@ -58,36 +50,24 @@ class EventLogDispatcher(EventDispatcher):
             return
 
         try:
-            self._get_dispatch(
-                worker_ctx, source_service=self.SOURCE_SERVICE
-            )(event_type=self.ENTRYPOINT_FIRED)
+            self._get_dispatch(worker_ctx)(event_type=self.ENTRYPOINT_FIRED)
         except Exception as exc:
             log.error(exc)
 
     def get_dependency(self, worker_ctx):
         return self._get_dispatch(worker_ctx)
 
-    def _get_dispatch(self, worker_ctx, source_service=None):
+    def _get_dispatch(self, worker_ctx):
         headers = self.get_message_headers(worker_ctx)
         kwargs = self.kwargs
         dispatcher = event_dispatcher(self.config, headers=headers, **kwargs)
 
-        if source_service is None:
-            source_service = self.service_name
-
-        body = self._get_base_call_info(worker_ctx)
-
         def dispatch(event_type, event_data=None):
+            body = self._get_base_call_info(worker_ctx)
             body['timestamp'] = _get_formatted_utcnow()
             body['event_type'] = event_type
             body['data'] = event_data or {}
-
-            if event_type == self.ENTRYPOINT_FIRED:
-                disp_event_type = self.ENTRYPOINT_FIRED
-            else:
-                disp_event_type = self.EVENT_TYPE
-
-            dispatcher(source_service, disp_event_type, body)
+            dispatcher(self.service_name, self.EVENT_TYPE, body)
 
         return dispatch
 
@@ -100,13 +80,6 @@ class EventLogDispatcher(EventDispatcher):
             'call_id': worker_ctx.call_id,
             'call_stack': deepcopy(worker_ctx.call_id_stack),
         }
-
-    def _declare_additional_exchange(self):
-        common_exchange = get_event_exchange(self.SOURCE_SERVICE)
-        if common_exchange is not None:
-            verify_amqp_uri(self.amqp_uri)
-            with get_connection(self.amqp_uri) as conn:
-                maybe_declare(common_exchange, conn)
 
     def _should_dispatch(self, entrypoint):
         return (
--- test/unit/test_dependency_provider.py
+++ test/unit/test_dependency_provider.py
@@ -1,7 +1,6 @@
 from unittest.mock import call, patch
 
 import pytest
-from kombu import Exchange
 
 from nameko_eventlog_dispatcher import EventLogDispatcher
 from nameko_eventlog_dispatcher.eventlog_dispatcher import EventDispatcher
@@ -12,13 +11,6 @@ class TestEventLogDispatcherSetup:
     @pytest.fixture
     def super_setup_mock(self):
         with patch.object(EventDispatcher, 'setup') as m:
-            yield m
-
-    @pytest.fixture
-    def maybe_declare_mock(self):
-        with patch(
-            'nameko_eventlog_dispatcher.eventlog_dispatcher.maybe_declare'
-        ) as m:
             yield m
 
     def test_setup_with_default_params(self, mock_container, super_setup_mock):
@@ -66,22 +58,3 @@ class TestEventLogDispatcherSetup:
         assert dependency_provider.entrypoints_to_exclude == [
             'test_1', 'test_2'
         ]
-
-    def test_setup_declares_exchange_when_set_to_auto_capture(
-        self, mock_container, super_setup_mock, config, maybe_declare_mock
-    ):
-        config['EVENTLOG_DISPATCHER'] = {
-            'auto_capture': True,
-            'entrypoints_to_exclude': ['test_1', 'test_2'],
-        }
-        mock_container.config = config
-        dependency_provider = EventLogDispatcher().bind(
-            mock_container, 'eventlog_dispatcher'
-        )
-
-        dependency_provider.setup()
-
-        exchange = maybe_declare_mock.call_args_list[0][0][0]
-        assert maybe_declare_mock.call_count == 1
-        assert isinstance(exchange, Exchange)
-        assert exchange.name == 'all.events'
