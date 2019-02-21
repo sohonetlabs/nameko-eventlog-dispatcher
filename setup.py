d63c275bbaa644cf8ae7b68efc3be5520f0c0c89
Julio Trigo <julio.trigo@gmail.com>
2019-02-21 14:20:06 +0000

    Update package classifiers

--- setup.py
+++ setup.py
@@ -33,16 +33,15 @@ setup(
     zip_safe=True,
     license='MIT License',
     classifiers=[
-        "Programming Language :: Python",
-        "Operating System :: POSIX",
-        "Operating System :: MacOS :: MacOS X",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Intended Audience :: Developers",
     ]
 )
